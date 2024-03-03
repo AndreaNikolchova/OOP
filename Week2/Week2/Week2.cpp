67b3ef3514672e8e8109ac35442897b7a278b189
AndreaNikolchova <a.nikolchova04@gmail.com>
2024-03-03 10:35:18 +0200

    First task done!

---
+++ Week2/Week2/Week2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+
+void readThreeNumbers() {
+
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	int sum = a + b + c;
+	int mult = a * b * c;
+
+	std::ofstream outFile("result.txt");
+
+	if (!outFile.is_open()) {
+		std::cout << "Error opening file!" << std::endl;
+		return;
+	}
+	outFile << sum << " " << mult << std::endl;
+	outFile.close();
+}
+int subtractNumbersFromFile() {
+	std::ifstream inFile("result.txt");
+
+	if (!inFile.is_open()) {
+		std::cout << "Error opening file!" << std::endl;
+		return -1;
+	}
+	int sum, mult;
+	inFile >> sum >> mult;
+
+	inFile.close();
+	return sum - mult;
+}
+
+int main()
+{
+	//readThreeNumbers();
+	std::cout<<subtractNumbersFromFile();
+}
+
