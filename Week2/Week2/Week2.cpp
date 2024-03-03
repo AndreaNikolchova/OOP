501d4a734bad302e9f74138cb369b4baebc3e23d
AndreaNikolchova <a.nikolchova04@gmail.com>
2024-03-03 13:11:37 +0200

    Second task done!

--- Week2/Week2/Week2.cpp
+++ Week2/Week2/Week2.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <fstream>
-
+//1
 void readThreeNumbers() {
 
 	int a, b, c;
@@ -30,9 +30,62 @@ int subtractNumbersFromFile() {
 	inFile.close();
 	return sum - mult;
 }
+//2
+const int REALTION_MAX_SIZE = 25;
+struct Pair {
+	int a;
+	int b;
+};
+struct Relation {
+	int size;
+	Pair pairs[REALTION_MAX_SIZE];
+};
+void writeRelationToFile(const char* fileName,const Relation& relation) {
+	std::ofstream ofs(fileName);
+	if (!ofs.is_open()) {
+		std::cout << "Error opening file!" << std::endl;
+		return;
+	}
+	for (size_t i = 0; i < relation.size; i++)
+	{
+		ofs << '<' << relation.pairs[i].a << ';' << relation.pairs[i].b << '>'<< std::endl;
+	}
+	ofs.close();
+}
+Relation readRelationFromFile(const char* fileName) {
+	std::ifstream ifs(fileName);
+	if (!ifs.is_open()) {
+		std::cout << "Error opening file!" << std::endl;
+		return {0,0};
+	}
+	
+	Relation relation;
+	relation.size = 0;
+	while (true) {
+
+		if (ifs.eof()) {
+			break;
+		}
+		char open, bettween , close ;
+		int a , b ;
+		ifs >> open >> a >> bettween >> b >> close;
+		Pair currentPair = {a,b};
+		relation.pairs[relation.size++] = currentPair;
+	}
+	ifs.close();
+	return relation;
+}
+void addPairToRelation(const Pair& pair, Relation& relation) {
+	if (relation.size + 1 > 25) {
+		std::cout << "Can't add pair, because the size of the relation is already 25" << std::endl;
+		return;
+	}
+	relation.size++;
+	relation.pairs[relation.size] = pair;
+ }
 
 int main()
 {
-	
+	Relation rl = readRelationFromFile("relation.txt");
 }
 
